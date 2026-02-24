01f50c22eceb242af0c904a1adf2c2d257641193
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-24 21:59:22 +0100

    update

---
+++ backend/src/SubSnap.Application/Abstractions/ISubscriptionRepository.cs
@@ -0,0 +1,17 @@
+﻿using SubSnap.Core.Domain.Aggregates;
+using SubSnap.Core.Domain.Entities;
+using SubSnap.Core.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubSnap.Core.Contracts.Repositories;
+
+public interface ISubscriptionRepository
+{
+    Task<Subscription?> GetByIdAsync(SubscriptionId id);
+    Task<SubscriptionAggregate?> GetAggregateAsync(SubscriptionId id);
+    Task AddAsync(Subscription subscription);
+}
---
+++ backend/src/SubSnap.Application/Abstractions/IUserAggregateLoader.cs
@@ -0,0 +1,17 @@
+﻿using SubSnap.Core.Domain.Aggregates;
+using SubSnap.Core.Domain.Entities;
+using SubSnap.Core.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubSnap.Core.Contracts.Queries;
+
+public interface IUserAggregateLoader
+{
+    Task<UserSubscriptionsAggregate?> LoadWithSubscriptions(UserId userId, CancellationToken ct = default);
+    //Task<List<Subscription>> LoadSubscriptions(UserId userId, CancellationToken ct);
+
+}
