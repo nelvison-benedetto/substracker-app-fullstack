2402d502f11c75b877db4d2021a4dff66359cfeb
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-26 15:18:08 +0100

    update

---
+++ backend/src/SubSnap.Application/UseCases/Auth/Login/Policies/PasswordPolicy.cs
@@ -0,0 +1,20 @@
+﻿using SubSnap.Application.Ports.Auth;
+using SubSnap.Core.Domain.Entities;
+
+namespace SubSnap.Application.UseCases.Auth.Login.Policies;
+
+public sealed class PasswordPolicy
+{
+    private readonly IPasswordHasherService _hasher;
+
+    public PasswordPolicy(IPasswordHasherService hasher)
+    {
+        _hasher = hasher;
+    }
+
+    public void EnsureValid(string password, User user)
+    {
+        if (!_hasher.Verify(password, user.PasswordHash))
+            throw new UnauthorizedAccessException();
+    }
+}
