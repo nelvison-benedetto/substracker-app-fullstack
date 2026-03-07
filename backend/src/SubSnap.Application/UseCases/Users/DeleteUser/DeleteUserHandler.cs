23c77001fb00e656e3327c7172c3c609aea794e7
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-07 12:03:59 +0100

    update

---
+++ backend/src/SubSnap.Application/UseCases/Users/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SubSnap.Application.UseCases.Users.GetUsersWithSubscriptions;
+
+namespace SubSnap.Application.UseCases.Users.DeleteUser;
+
+public sealed class DeleteUserHandler
+{
+
+}
---
+++ backend/src/SubSnap.Application/UseCases/Users/DeleteUser/DeleteUserOrchestrator.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SubSnap.Application.UseCases.Users.GetUsersWithSubscriptions;
+
+namespace SubSnap.Application.UseCases.Users.DeleteUser;
+
+//wrappa l'handler, E' ENTRY POINT application!! è chiamato da plugin MediatR, dice 'Io sono il punto di ingresso dell’Application Layer per questo use case.'. 
+//QUINDI NEL CONTROLLER FAI  await _gusOrchestrator.Execute(ct);, NO await _gusOrchestrator.Execute(ct); xk poi difficile cambiare orchestrazione, MEGLIO SE IL CONTROLLER NON CONOSCE mediatr!!
+//Controller → Orchestrator → MediatR pipelibe behviors → Handler
+
+public sealed class DeleteUserOrchestrator
+{
+    private readonly IMediator _mediator;
+
+    public DeleteUserOrchestrator(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public Task<List<GUSResult>> Execute(
+        CancellationToken ct = default)
+    {
+        return _mediator.Send(new DeleteUserCommand(), ct);  //QUI PARTE TUTTA LA PIPELINE mediatr cioe TUTTI I BEHAVIORS...fino all'handler!!
+    }
+}
