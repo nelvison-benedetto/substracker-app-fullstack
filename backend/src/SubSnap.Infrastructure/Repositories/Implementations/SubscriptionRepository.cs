fc3a48a25171cb3272a8be76d10c139c97eb4be4
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-10 11:37:47 +0100

    update

---
+++ backend/src/SubSnap.Infrastructure/Repositories/Implementations/SubscriptionRepository.cs
@@ -0,0 +1,26 @@
+﻿using SubSnap.Core.Domain.Entities;
+using SubSnap.Infrastructure.Persistence.Context;
+
+namespace SubSnap.Infrastructure.Repositories.Implementations;
+
+public class SubscriptionRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public SubscriptionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task AddAsync(Subscription subscription, CancellationToken ct)
+    {
+        _context.Subscriptions.Add(subscription);
+        return Task.CompletedTask;
+    }
+    public Task DeleteAsync(Subscription subscription, CancellationToken ct)
+    {
+        _context.Subscriptions.Remove(subscription);
+        return Task.CompletedTask;
+    }
+
+}
